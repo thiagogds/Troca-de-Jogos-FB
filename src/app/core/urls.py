a7407012db74bad76ae689c7e855ed697d079e8a
Thiago Garcia <thiagogds14@gmail.com>
2011-12-22 17:23:52 -0200

    Tira o CSRF da view usada no fb.

--- src/app/core/urls.py
+++ src/app/core/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
+    url(r'^$', csrf_exempt(TemplateView.as_view(template_name='core/home.html')), name='home'),
 )
 
