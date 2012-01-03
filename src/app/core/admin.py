a9f6dde142e4148a6483e4bca4ea55f2b577f154
Thiago Garcia <thiagogds14@gmail.com>
2012-01-03 18:14:18 -0200

    Adiciona modelo de jogos ao admin. Com fotinha :D

---
+++ src/app/core/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from models import Game
+
+from lib.decorators import admin_attrs
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['cover_image', 'name']
+
+    @admin_attrs(allow_tags=True, short_description=u"Capa", admin_order_field=u"cover")
+    def cover_image(self, obj):
+        return "<img src='%s' style='max-width: 200px;'>" % (obj.cover)
+
+admin.site.register(Game, GameAdmin)
