077986fbf5e12cb6b94b7977e51da1b03fcb48f4
Thiago Garcia <thiagogds14@gmail.com>
2012-01-31 13:18:27 -0200

    apaga contrib desnecessário

--- contrib/alunos.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# Obtido com o comando:
-#
-# >>> from pprint import pprint
-# >>> from app.subscription.models import Susbcription
-# >>> pprint(list(Subscription.objects.all().values()))
-
-import datetime
-alunos = []
